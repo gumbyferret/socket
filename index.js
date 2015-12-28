a4a56637e9b86795aadaf1498d4d7c574fb595c8
gumbyferret <yb@converged-systems.com>
2015-12-28 08:33:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-app.listen(80);
+//app.listen(80);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
@@ -13,4 +13,8 @@ io.on('connection', function (socket) {
   socket.on('my other event', function (data) {
     console.log(data);
   });
+});
+
+http.listen(80, function(){
+  console.log('listening on 192.168.1.101:3000');
 });
