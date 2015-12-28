eaec1692d021aa48d1e8e59dff5171398798ea30
gumbyferret <yb@converged-systems.com>
2015-12-28 08:38:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  socket.emit('news', { hello: 'world', test: 'new data' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
